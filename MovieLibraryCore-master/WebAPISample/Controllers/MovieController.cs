b1680d179a72e32b6ae93bb50c267ac0c74a27f8
danamurphy48 <danamurphy48@gmail.com>
2020-06-02 15:53:06 -0500

    Get all movies from database functions properly.

--- MovieLibraryCore-master/WebAPISample/Controllers/MovieController.cs
+++ MovieLibraryCore-master/WebAPISample/Controllers/MovieController.cs
@@ -24,7 +24,7 @@ namespace WebAPISample.Controllers
         {
             // Retrieve all movies from db logic
             var movie = _context.Movies.ToList();
-            return Ok(new string[] { "movie1 string", "movie2 string" });
+            return Ok(movie);
         }
 
         // GET api/movie/5
